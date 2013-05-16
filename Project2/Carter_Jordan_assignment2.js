dd880cbd9211d3881c8603738de7c17c7f3dffa4
Jordan Carter <jcarter2fullsail.edu>
2013-05-16 18:46:22 -0400

    added other function

--- Project2/Carter_Jordan_assignment2.js
+++ Project2/Carter_Jordan_assignment2.js
@@ -5,5 +5,51 @@
 
 
 //setting up my variables
+var hero = "Jimi";
+var CoastIsClear = "no one is looking";  
+var sneaking =3;
+var rightCombination = [45,17,28,68];
 
-var Hero = Jimi
+//Process procedure
+
+var OpeningCellDoor = function(LookingOut){
+    
+    if ("no one is looking") {
+        console.log ("the prison door opens and " + hero + " leaves the cell")
+    } else console.log ("door is still shut")
+}
+
+//boolean function
+
+var movingInTheHall = function( noiseLevel, anyoneLooking){
+	if(noiseLevel < 5 && anyoneLooking == "no one is looking" ){
+		return console.log ("Moving past the hall!");
+	
+	} else return console.log ("busted!");
+		
+}
+
+//number Function w/ While loop
+	var PrisonLevels = -6
+	
+	while(PrisonLevels < 0){	
+		PrisonLevels++;
+		console.log(PrisonLevels + " Floors to Go");
+		 
+		};
+		
+//Array Function w/ for-loop
+
+var startingNumbers = [15,0,15,45]
+		
+for (i=0; i<5; i++) {};
+
+
+//Main code - inputs
+OpeningCellDoor (CoastIsClear);
+
+movingInTheHall (sneaking , CoastIsClear);
+
+
+console.log( PrisonLevels + " floors to go, almost home!")
+
