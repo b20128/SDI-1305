a2890d2f36e13b005996297bb6163ed71a7954ad
Jordan Carter <jcarter2fullsail.edu>
2013-05-22 08:29:52 -0400

    Added project 3, added the first object.

---
+++ project_3/Carter_Jordan_assisignment3.js
@@ -0,0 +1,34 @@
+//Jordan Carter
+//SDI-1305
+//project 3
+//5/21/13
+ 
+var storyHero = "Jimi the giant"
+var lastSeenIn =
+
+//created 1st object
+var MainCharacter = {
+    "name" : storyHero,
+    "description" : [
+        "4 foot even ",
+        " wearing an orange jumpsuit ",
+        "armed and extreamly dangerous"
+    ],
+    "apb": function () {
+        var allPoints = this.name + this.description + "headed east on highawasee";
+        
+        return allPoints;
+    },
+    "escaping" : function(currentEscapingIn){
+        
+        
+    }
+
+    
+    
+}
+
+
+console.log(allPoints);
+
+
