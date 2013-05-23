39b0e7bc6887514e2eab79df9b4c44840631c201
Jordan Carter <jcarter2fullsail.edu>
2013-05-23 12:01:19 -0400

    had to re-edit after a bad merge

--- project_3/Carter_Jordan_assisignment3.js
+++ project_3/Carter_Jordan_assisignment3.js
@@ -7,7 +7,7 @@
 //variables
 var storyHero = "Jimi the giant"
 var lastSpottedIn = " a toyota camry"
-var daysSinceEscape = 7
+var daysSinceEscape
 var beOntheLookout
 var alertTheMasses
 
@@ -19,45 +19,35 @@ var MainCharacter = {
 	"wearing ": "Orange Jumpsuit",
 	"armed ": true
     },
-<<<<<<< HEAD
-=======
-    var Spotted: function(currentEscapingIn){
-    	var BreakingNews =("suspect seen in " + lastSPottedIn)
-    	return BreakingNews
-    	
-    }
 
-    // i cant get either of these methods to work what am i missing?
-    // i'm trying to incorporate a loop some where in all this,
-    // but i'm drawing a complete blank. How could i make use of it?
-    // also i wanted to have like a special order for the police that pops up if 
-    // the hero is armed, in the console, how should i do that and what would that be in the flow chart?
-}
+     
 //main code 
->>>>>>> c60b2d938e77c48d3cca48137ba8ff16595610b9
 
-    //My methods
-    "APBs": function(daysSinceEscape){
-	if (daysSinceEscape < 7) {
-	var beOntheLookout = this.name + this.description + this.armed;
-	 
+    //My methods, APBs keeps giving me a "missing formal parameter:(on colum 23)" error
+    //can a loop be a function, if so what am i missing this should work shouldn't it??
+    "APBs": function (for (daysSinceEscape = 0;daysSinceEscape >== 3; daysSinceEscape = daysSinceEscape + 1)){
+	if (daysSinceEscape < 3){
+	    var beOntheLookout = this.name + this.description + this.armed;}
+		else var alertTheMasses = storyHero + " has escaped!!";
     }
-	else var alertTheMasses = storyHero + " has escaped!!";
-	
-    },
+},
+    
+
+
+    "newsUpdate": function(currentEscapingIn) {
+	var BreakingNews = ("be on the lookout for " + storyHero + "suspect last seen in" + lastSpottedIn);
+	return BreakingNews;
+    }
+
+};
+console.log(beOntheLookout||alertTheMasses);
+
+console.log(BreakingNews);
 //I'm trying to create a method called APBs that runs a console log
 //that either tells the police what to look for or alerts the public 
 //I'd like to run the console at the end, i cant get it working either way
 //should i be running the functions/methods? i felt like i was blending the different
 //data types together and after you last email, thats looking more and more
 //probable.
+
 //thanks for the help!
-
-    "newsUpdate": function(currentEscapingIn) {
-	var BreakingNews = ("suspect seen in " + lastSpottedIn);
-	return BreakingNews;
-    }
-
-};
-console.log(beOntheLookout);
-console.log(alertTheMasses);
