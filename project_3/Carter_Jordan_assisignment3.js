a8d27073a6caca9554c0eaabdd4d056ec6fb5676
Jordan Carter <jcarter2fullsail.edu>
2013-05-23 09:17:41 -0400

    1 step forward 2 steps back

--- project_3/Carter_Jordan_assisignment3.js
+++ project_3/Carter_Jordan_assisignment3.js
@@ -2,36 +2,46 @@
 //SDI-1305
 //project 3
 //5/21/13
- 
+
+
+//variables
 var storyHero = "Jimi the giant"
 var lastSpottedIn = " a toyota camry"
+var daysSinceEscape = 7
+var beOntheLookout
+var alertTheMasses
 
 //created 1st object
 var MainCharacter = {
-    "name" : storyHero,
-    "description" : [
-        "Height": 3.5,
-        " wearing": "Orange Jumpsuit",
-        "armed": true
-    ],
-    var apb: function () {
-        var allPoints = this.name + this.description + this.armed;
-        return allPoints;
+    "name": storyHero,
+    "description": {
+	"height ": 3.5,
+	"wearing ": "Orange Jumpsuit",
+	"armed ": true
     },
-    var Spotted: function(currentEscapingIn){
-    	var BreakingNews =("suspect seen in " + lastSPottedIn)
-    	return BreakingNews
-    	
+
+    //My methods
+    "APBs": function(daysSinceEscape){
+	if (daysSinceEscape < 7) {
+	var beOntheLookout = this.name + this.description + this.armed;
+	 
+    }
+	else var alertTheMasses = storyHero + " has escaped!!";
+	
+    },
+//I'm trying to create a method called APBs that runs a console log
+//that either tells the police what to look for or alerts the public 
+//I'd like to run the console at the end, i cant get it working either way
+//should i be running the functions/methods? i felt like i was blending the different
+//data types together and after you last email, thats looking more and more
+//probable.
+//thanks for the help!
+
+    "newsUpdate": function(currentEscapingIn) {
+	var BreakingNews = ("suspect seen in " + lastSpottedIn);
+	return BreakingNews;
     }
 
-    // i cant get either of these methods to work what am i missing?
-    // i'm trying to incorporate a loop some where in all this,
-    // but i'm drawing a complete blank. How could i make use of it?
-    // also i wanted to have like a special order for the police that pops up if 
-    // the hero is armed, in the console, where should i look for that in the flowchart?
-}
-//main code 
-
-console.log(allPoints);
-
-
+};
+console.log(beOntheLookout);
+console.log(alertTheMasses);
