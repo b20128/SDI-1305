35f085291991a3d4da0b25fdb3fe142f2d7c7b05
Jordan Carter <jcarter2fullsail.edu>
2013-05-24 00:41:58 -0400

    finally getting the hang of it, i've got a fully working object with methods.

--- project_3/Carter_Jordan_assisignment3.js
+++ project_3/Carter_Jordan_assisignment3.js
@@ -7,42 +7,47 @@
 //variables
 var storyHero = "Jimi the giant"
 var lastSpottedIn = " a toyota camry"
-var daysSinceEscape
-var beOntheLookout
-var alertTheMasses
+var armed = true 
 
 //created 1st object
 var MainCharacter = {
     "name": storyHero,
-    "description": {
-	"height ": 3.5,
-	"wearing ": "Orange Jumpsuit",
-	"armed ": true
-    },
+    "description": [
+	" height: 3.5",
+	" wearing an orange jumpsuit",
+	" Armed and Dangerous"
+	],
+    
+ 
 
-     
-//main code 
-
-    //My methods, APBs keeps giving me a "missing formal parameter:(on colum 23)" error
-    //can a loop be a function, if so what am i missing this should work shouldn't it??
-    "APBs": function (for (daysSinceEscape = 0;daysSinceEscape >== 3; daysSinceEscape = daysSinceEscape + 1)){
-	if (daysSinceEscape < 3){
-	    var beOntheLookout = this.name + this.description + this.armed;}
-		else var alertTheMasses = storyHero + " has escaped!!";
+    
+    "APBs": function (daysSinceEscape){
+	
+	if (daysSinceEscape > 3){
+	    var beOntheLookout = this.name + this.description;}
+	    else {var PanicAtTheJail = storyHero + " has escaped!!"};
+	    console.log(beOntheLookout||PanicAtTheJail);
+ 	},
+    
+    "UseOfForce": function(armed){
+	if(true){
+	    var CurrentOrders = "Shoot to kill"
+	}
+	else{ 
+	   var CurrentOrders = "Take him in alive"    
+	};
+	return CurrentOrders;
     }
-},
+};
     
 
+var UseOfForceOrder = MainCharacter.UseOfForce(armed)
+MainCharacter.APBs(7);
+console.log(UseOfForceOrder)
 
-    "newsUpdate": function(currentEscapingIn) {
-	var BreakingNews = ("be on the lookout for " + storyHero + "suspect last seen in" + lastSpottedIn);
-	return BreakingNews;
-    }
 
-};
-console.log(beOntheLookout||alertTheMasses);
 
-console.log(BreakingNews);
+
 //I'm trying to create a method called APBs that runs a console log
 //that either tells the police what to look for or alerts the public 
 //I'd like to run the console at the end, i cant get it working either way
