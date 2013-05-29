232afbc23a53520faa34a2e671f7f667d37313ec
Jordan Carter <jcarter2fullsail.edu>
2013-05-29 14:25:29 -0400

    gonna move on to next problem until i get some help as far as why its not working

--- project4/Carter_Jordan_assignment4.js
+++ project4/Carter_Jordan_assignment4.js
@@ -9,33 +9,40 @@
 StringLib = {
 //Is this Number a Phone Number (abc-def-ghij) ?
 // problem 1 starts
-"PhoneNumCheck":function(TenDigits){
-        var firstCheck = TenDigits.indexOf("-");
-        var secondCheck = TenDigits.lastIndexOf("-");
-        if (TenDigits.length === 12){
-            if ( firstCheck == 3 && secondCheck == 7) {
-            return true   
-            }else{
-            return false} 
-        }else{return false}
-        
-        
-        
+    "PhoneNumCheck":function(TenDigits){
+            var firstCheck = TenDigits.indexOf("-");
+            var secondCheck = TenDigits.lastIndexOf("-");
+            if (TenDigits.length === 12){
+                if ( firstCheck == 3 && secondCheck == 7) {
+                return true   
+                }else{
+                return false} 
+            }else{return false}
+            
+            
+            
     },//problem 1 ends. Running smooth, DONT TOUCH!!
 
 
 //Is this String an email adress?
 //problem 2 begins
-"emailCheck": function(emailadress){
+    "EmailCheck": function(emailAdress){
+        var dot = emailAdress.lastindexOf(".");
+        var atSign = emailAdress.indexOf("@") 
+        if (emailAdress.carAt(dot,length) === ".com"||".edu"||".gov"){
+            if (atSign > -1) {
+            return true
+            } else{return false}
     
-    }
-};
-
-//problem 2 ends
+        }
+    }//problem 2 ends
 
 //problem 3 begins
+};
+
 
 
 
 //main code calls
 console.log(StringLib.PhoneNumCheck("610-262-5500"));
+console.log(StringLib.EmailCheck("zenmaster501@gmail.com"))
