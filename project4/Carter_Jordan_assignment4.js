34648c196f8025bd462d88db9aa371d90fa57d64
Jordan Carter <jcarter2fullsail.edu>
2013-05-29 02:36:48 -0400

    first function made

---
+++ project4/Carter_Jordan_assignment4.js
@@ -0,0 +1,20 @@
+alert("JavaScript works!");
+
+//string functions
+
+//problem 1 starts
+var PhoneNumberfunc = function (TenDigits){
+    for([i]=0;i< TenDigits.length; i=i++);
+    isNaN(TenDigits);
+    if ([3] ==="-" && [7]==="-") {
+        return true
+    } else{
+        return false
+    }
+    
+}//problem 1 ends
+
+
+//problem 2 begins
+
+//problem 2 ends
