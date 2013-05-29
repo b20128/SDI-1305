148bce33234c69d4cc2a645c869394ac46040fc8
Jordan Carter <jcarter2fullsail.edu>
2013-05-29 18:41:28 -0400

    going strong, got problem 4 out of the way easier than i expected, i feel it will be trial and error from here on out. i'm gonna give it my best.

--- project4/Carter_Jordan_assignment4.js
+++ project4/Carter_Jordan_assignment4.js
@@ -27,9 +27,9 @@ StringLib = {
 //Is this String an email adress?
 //problem 2 begins
     "EmailCheck":function(emailAdress){
-        var Dotcom = emailAdress.substring(emailAdress.length,emailAdress.length-4)
+        var Dotcom = emailAdress.substring(emailAdress.length , emailAdress.length - 4)
         var atSign = emailAdress.indexOf("@") 
-        if (Dotcom == ".com"||".edu"||".gov"){
+        if (Dotcom === ".com"||".edu"||".gov"){
             if (atSign > -1) {
             return true
             } else{return false}
@@ -41,20 +41,32 @@ StringLib = {
 //problem 3 begins
     "SiteCheck":function(webSitename){
         if( (webSitename.indexOf("https://")==0) ||
-           (webSitename.indexOf("http://")==0) ||
-           (webSitename.indexOf("ftp:")==0) ){
+           (webSitename.indexOf("http://")==0)) {
             return true
            }
         else{return false};
+    },//problem 3 ends
+    
+    "capEachWord":function(capThis){
+        var words = capThis.split(" ");
+        var arr = Array();
+        for (i in words)
+        {
+            lowCase = words[i].toLowerCase();
+            lowCase = lowCase.charAt(0).toUpperCase() + lowCase.substring(1);
+        arr.push(lowCase);
+        }var corrected = arr.join(" ") 
+        return corrected;
     }
 
+    
+
+
 };
-
-
 
 
 //main code calls
 console.log(StringLib.PhoneNumCheck("610-262-5500"));
 console.log(StringLib.EmailCheck("zenmaster501@gmail.com"));
 console.log(StringLib.SiteCheck("https://www.fullsail.com"));
-
+console.log(StringLib.capEachWord("hey everyone!"))
